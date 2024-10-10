02b022d25927090337794565a825ca413e09d261
Levy-FernandesPixel <levylimafernandes@gmail.com>
2024-10-10 16:27:44 -0300

    correção de bugs/ test FrontEnd

---
+++ Sistema_Veiculo/frontend/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1 id = "titulo">Mecanica de carros</h1>
+    </header>
+</template>
+
+<script>
+export default{
+    name: 'AppHeader',
+}
+</script>
+
+
+<style scoped>
+
+#titulo {
+color: black;
+font-family: Arial, Helvetica, sans-serif;
+}
+</style>
