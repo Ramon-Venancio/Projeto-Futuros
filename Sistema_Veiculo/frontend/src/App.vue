23161f3f3e7ce86c715d3c85c80f315ed0509e8b
Levy-FernandesPixel <levylimafernandes@gmail.com>
2024-10-15 16:24:27 -0300

    frontEnd-header pronto

--- Sistema_Veiculo/frontend/src/App.vue
+++ Sistema_Veiculo/frontend/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
   <Header />
+  <Navbar />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Navbar
   }
 }
 </script>
 
 <style>
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
--- Sistema_Veiculo/frontend/src/components/Header.vue
+++ Sistema_Veiculo/frontend/src/components/Header.vue
@@ -1,20 +1,53 @@
 <template>
-    <header>
-        <h1 id = "titulo">Mecanica de carros</h1>
+    <header> 
+        <img :src="logo" alt="Logo da Empresa" id="imagem">
+        <h1 class="Titulo">Gestao de veiculos</h1>
+        <div class="Logout"> </div>
     </header>
 </template>
 
 <script>
 export default{
     name: 'AppHeader',
+    data(){
+        return{
+            logo: require('@/assets/logo.png')
+
+        }
+    }
 }
+
 </script>
 
 
 <style scoped>
 
 #titulo {
-color: black;
+color: white;
 font-family: Arial, Helvetica, sans-serif;
+}
+
+header{
+margin: 0px 20%;
+background-color: #007192;
+padding: 1rem;
+border-radius: 10px 10px 0 0;
+box-shadow: 0px 5px 20px 5px rgba(0,0,0,0.2);
+display: flex;
+justify-content: center;
+align-items: center; 
+}
+
+#imagem{
+    width: 100px;
+    height: 100px;
+    display: block;
+    margin-right: 30px;
+    
+}
+
+.Titulo{
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 </style>
